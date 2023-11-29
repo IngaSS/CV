3e933865f524bb65a9b0310140e818fb73f05bb6
Инга Гулина <gulina@nanosemantics.ai>
2023-11-29 18:01:34 +0300

    feat(project): dist folder save

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+// const API_PROD = 'https://my-cv-cms.onrender.com/graphql'
+
 export const client = new ApolloClient({
   uri: 'http://localhost:1337/graphql',
   cache: new InMemoryCache(),
