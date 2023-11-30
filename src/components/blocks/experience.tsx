80989fd3b510382a466185df7eb7f5c2e931cddc
Инга Гулина <gulina@nanosemantics.ai>
2023-11-30 11:06:31 +0300

    feat(project): build

--- src/components/blocks/experience.tsx
+++ src/components/blocks/experience.tsx
@@ -14,7 +14,10 @@ const Experience = memo(({ title, events }: any) => {
   const markdownContent = useMemo(() => data?.main?.data?.attributes, [data?.main?.data?.attributes])
 
   const editedEvents = useMemo(
-    () => events?.map((el: any) => (el.markdown ? { ...el, markdown_text: markdownContent[el.markdown] || '' } : el)),
+    () =>
+      events?.map((el: any) =>
+        el.markdown ? { ...el, markdown_text: markdownContent ? markdownContent[el.markdown] : '' } : el
+      ),
     [events, markdownContent]
   )
 
